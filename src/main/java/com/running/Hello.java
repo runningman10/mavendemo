ec536b677675927332ed76a894c85c6a7fb91028
runningman <dlrunning1024@163.com>
2017-07-04 09:33:22 +0800

    项目

---
+++ src/main/java/com/running/Hello.java
@@ -0,0 +1,10 @@
+package com.running;
+
+/**
+ * Created by dlrun on 2017/7/3.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("Hello,world");
+    }
+}
