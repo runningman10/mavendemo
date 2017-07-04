cf68e58657b9ed124de1af0f302b03ea1ffb0458
runningman <dlrunning1024@163.com>
2017-07-04 09:30:39 +0800

    你好，世界

---
+++ src/main/java/com/running/HelloDemo.java
@@ -0,0 +1,10 @@
+package com.running;
+
+/**
+ * Created by dlrun on 2017/7/4.
+ */
+public class HelloDemo {
+    public static void main(String[] args) {
+        System.out.print("你好，世界");
+    }
+}
